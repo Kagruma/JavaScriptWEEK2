cc9600ff6a0b1f9d72f76d1a8140961da757dc81
Kagruma <nguyene375@gmail.com>
2020-11-06 12:00:07 -0500

    finished easy challenge

---
+++ Easy/scripts.js
@@ -0,0 +1,7 @@
+var lastName = ["Pelletier","Milde","Ross"]
+
+var firstName = ["Maya", "Nicky","Riley"]
+
+var age = ["18", "20", "19"]
+
+console.log("Hello, my name is " + firstName[1] +" " + lastName[1] + " and I'm " + age[1]);
